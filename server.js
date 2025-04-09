9c32645c39021372e63ca909749b818a943fee98
Clifford <fordbajan05@gmail.com>
2025-04-09 23:56:48 +0800

    updated add books route with insert query

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.get("/books", async (req, res) => {
 
 app.get("/search", async (req, res) => {
   // search route for searching books to the api
+
   const query = req.query.search_input;
   if (!query) {
     return res.status(400).json({ error: "Search query is required" });
@@ -46,6 +47,8 @@ app.get("/search", async (req, res) => {
       query
     )}`;
     const result = await axios.get(url);
+    const workID = result.data.docs[0].key;
+    const bookId = workID.split("/").pop();
 
     const books = result.data.docs.map(async (book) => {
       //map book the data to the required format
@@ -55,6 +58,8 @@ app.get("/search", async (req, res) => {
           const workUrl = `https://openlibrary.org${book.key}.json`;
           const workResponse = await axios.get(workUrl);
           if (workResponse.data.description) {
+            // CHECK IF THE DESCRIPTION EXISTS
+            //if description is a text use it else if its not (object) use the value
             summary =
               typeof workResponse.data.description === "string"
                 ? workResponse.data.description
@@ -65,6 +70,7 @@ app.get("/search", async (req, res) => {
         }
       }
       return {
+        // returns properties of books
         title: book.title,
         author: book.author_name ? book.author_name[0] : "Unknown Author",
         year: book.first_publish_year || "Unknown Year",
@@ -73,6 +79,7 @@ app.get("/search", async (req, res) => {
           ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
           : null,
         summary: summary,
+        bookWork: bookId,
       };
     });
 
@@ -84,7 +91,26 @@ app.get("/search", async (req, res) => {
   }
 });
 
-app.post("/addBook", async (req, res) => {});
+app.post("/addBook", async (req, res) => {
+  try {
+    const date = new Date().toISOString();
+    // get body request data and destructure it
+    const { title, author, year, edition, summary, work } = req.body;
+    const coverId = req.body.coverId || null;
+
+    // Convert year to a proper date format (YYYY-01-01)
+    const formattedYear = year ? `${year}-01-01` : null;
+    // insert book to books db
+    await db.query(
+      "INSERT INTO books (book_title, author, book_year, edition, summary, cover, date_added, book_work) VALUES ($1, $2, $3, $4, $5, $6, $7,$8)",
+      [title, author, formattedYear, edition, summary, coverId, date, work]
+    );
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error adding book:", error);
+    res.status(500).json({ error: "Failed to add book" });
+  }
+});
 
 app.listen(port, () => {
   console.log("Server is running on port 3000");
